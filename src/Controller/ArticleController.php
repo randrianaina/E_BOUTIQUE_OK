1bfd7b7d4579bc2b38698c393f1546e17061ae3d
Leeloo <lydiebouvet@MacBook-Air-de-Lydie.local>
2020-09-14 18:45:09 +0200

    Ajout Leeloo

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Articles;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/article", name="article")
+     */
+    public function index()
+    {
+        return $this->render('article/index.html.twig', [
+            'controller_name' => 'ArticleController',
+        ]);
+    }
+
+    /**
+     * @Route("/article/tous", name="article_tous")
+     */
+    public function All() // pour trouver par l'attribut que l'on veut => name, price voire les 2
+    {
+        $repository = $this->getDoctrine()->getRepository(Articles::class);
+        $articles = $repository->findAll(); // $products = $repository->findBy(['name'=>'keyboard', 'price'=>'ASC']); => vient tout mettre dans un tableau
+        /* $products = $repository->findAll(); */
+        dump($articles);
+
+        if (!$articles) {
+            throw $this->createNotFoundException('Pas d\'article trouvé ...!');
+        }
+
+        return $this->render('article/index.html.twig', ['articles' => $articles, 'controller_name' => 'Article Controller' ]);
+
+        // or render a template
+        // in the template, print things with {{ product.name }}
+        // return $this->render('product/show.html.twig', ['product' => $product]);
+    }
+
+}
