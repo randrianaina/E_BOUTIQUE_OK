cc603faeb8e70dbb5c880c80fbaefa7569103185
Horia Gheorghe <horia_dude@yahoo.com>
2020-09-15 16:42:59 +0200

    Entite Panier

---
+++ src/Entity/Panier.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PanierRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PanierRepository::class)
+ */
+class Panier
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function __construct(){
+		if(!isset($_SESSION['panier'])){
+			$_SESSION['panier'] = array();
+		}
+	}
+
+    public function add($product_id){
+		if(isset($_SESSION['panier'][$product_id])){
+			$_SESSION['panier'][$product_id]++;
+		}else{
+			$_SESSION['panier'][$product_id] = 1;
+		}
+	}
+}
